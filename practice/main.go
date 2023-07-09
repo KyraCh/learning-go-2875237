46804e26332e5e72aeb9be18a2f5ae3e993bf92f
KyraCh <92988384+KyraCh@users.noreply.github.com>
2023-07-09 22:07:32 +0300

    first changes

--- practice/main.go
+++ practice/main.go
@@ -4,33 +4,26 @@ import (
 	"fmt"
 )
 
-const aConst int = 64
+// special type of variable constsnt
+const aConst = 64 // can also be inferred like this, this is available to any function in the file
 
 func main() {
-
-	var aString string = "This is Go!"
+	var aString string = "This is Go"
 	fmt.Println(aString)
-	fmt.Printf("The variable's type is %T\n", aString)
-
-	var anInteger int = 42
+	fmt.Printf("The variable name is %T \n", aString) //%T is a verb or a placeholder
+	// Beware Printf does not add a line by default you need to add it manually
+	var anInteger int = 90
 	fmt.Println(anInteger)
 
-	var defaultInt int
+	var defaultInt int //all types have a defualt value for int it is 0
 	fmt.Println(defaultInt)
 
-	var anotherString = "This is another string"
-	fmt.Println(anotherString)
-	fmt.Printf("The variable's type is %T\n", anotherString)
+	var anotherString = "This is a value of another string" // this isn't explicitly defined
+	fmt.Printf("The variable name is %T \n", anotherString)
 
-	var anotherInt = 53
-	fmt.Println(anotherInt)
-	fmt.Printf("The variable's type is %T\n", anotherInt)
-
-	myString := "This is also a string"
+	// another style that you can use to initialise the varibale := no need to use var
+	// this only works insdie functions, outside you must use the var keyword
+	myString := "This is my string"
 	fmt.Println(myString)
-	fmt.Printf("The variable's type is %T\n", myString)
-
-	fmt.Println(aConst)
-	fmt.Printf("The variable's type is %T\n", aConst)
 
 }
